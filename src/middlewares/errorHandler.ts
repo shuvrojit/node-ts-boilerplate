6fcac7e3c8d5db14c8c50f9794c07b6cee79284c
Shuvrojit Biswas <jsuvro17@gmail.com>
2025-02-25 19:31:19 +0600

    feat(middlewares): add asyncHandler and errorHandler middlewares

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,62 @@
+import { Request, Response, NextFunction } from 'express';
+import ApiError from '../utils/ApiError';
+import config from '../config/config';
+import logger from '../config/logger';
+
+const sendErrorDev = (err: ApiError, res: Response) => {
+  res.status(err.statusCode).json({
+    status: 'error',
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err: ApiError, res: Response) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  } else {
+    // Programming or other unknown error: don't leak error details
+    logger.error('ERROR 💥', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+const errorConverter = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let error = err;
+  if (!(error instanceof ApiError)) {
+    const statusCode = error.statusCode || error instanceof Error ? 400 : 500;
+    const message = error.message || String(error);
+    error = new ApiError(statusCode, message, false, err.stack);
+  }
+  next(error);
+};
+
+const errorHandler = (
+  err: ApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+
+  if (config.env === 'development') {
+    sendErrorDev(err, res);
+  } else {
+    sendErrorProd(err, res);
+  }
+};
+
+export { errorConverter, errorHandler };
