f1631a6a73aca179260745fd6c9110117bcf5051
Shuvrojit Biswas <jsuvro17@gmail.com>
2025-02-25 19:33:21 +0600

    feat(app): add global error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Express, Response } from 'express';
+import express, { Express, Response, Request, NextFunction } from 'express';
+import { errorConverter, errorHandler } from './middlewares/errorHandler';
 import morganMiddleware from './config/morgan';
 
 const app: Express = express();
@@ -11,5 +12,9 @@ app.get('/', (_, res: Response) => {
   res.status(200);
   res.send('root');
 });
+
+// Global error handling middleware
+app.use(errorConverter);
+app.use(errorHandler);
 
 export default app;
